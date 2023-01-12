fd960835cc466f2645f8a1536514674a6b6a1087
sorna2108 <jagadeesansornalakshmi@gmail.com>
2023-01-12 12:49:04 +0530

    f4-commit

--- src/app/admin/addpizza/addpizza.component.ts
+++ src/app/admin/addpizza/addpizza.component.ts
@@ -49,19 +49,19 @@ export class AddpizzaComponent implements OnInit {
     formData.append('pizzaname', f.controls.pizzaname.value);
     formData.append('pizzasize', f.controls.pizzasize.value);
     formData.append('pizzaprice', f.controls.pizzaprice.value);
-    this.http.post<any>('https://database-wqgm.onrender.com/', formData).subscribe(
+    this.http.post<any>('https://database-wqgm.onrender.com/admin/addpizza', formData).subscribe(
       (res) => {
         this.adminService.avail = true;
         this.adminService.msg = "Successfully Added a pizza!!!"
         this.router.navigate(['/admin']);
-        // console.log(res)
+       console.log(res)
       }
       ,
       (error) => {
 
         if (error instanceof HttpErrorResponse) {
 
-            this.router.navigate(['/login'])
+            this.router.navigate(['/admin'])
 
         }
         console.log(error);
